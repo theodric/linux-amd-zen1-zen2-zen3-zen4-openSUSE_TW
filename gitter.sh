8f4e6c5de69e9e17592f4da980b588eea64103ef
þēodrīċ <theodric@users.noreply.github.com>
2024-12-27 18:45:36 +0000

    Switched gitter to use aria2c

--- gitter.sh
+++ gitter.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 KERNEL_URI=$(wget http://kernel.org -O - | grep 'tar.xz' | head -1 | cut -d "=" -f2 | sed -e 's/\"//g' | sed -e 's/>.*//g')
 KERNEL_FILE=$(echo $KERNEL_URI | sed -e 's/https.*\///g')
-wget $KERNEL_URI -O $KERNEL_FILE
+#wget $KERNEL_URI -O $KERNEL_FILE
+aria2c -x16 --out=$KERNEL_FILE $KERNEL_URI
