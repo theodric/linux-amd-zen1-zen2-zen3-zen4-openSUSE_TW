4e68489a020dbdf262aefc959439d7788c7d2c90
Theodric <theodric@github.io>
2025-01-17 15:09:10 +0000

    SHIT FUCK PISS ASS

--- zen1/maker-zen1.sh
+++ zen1/maker-zen1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-git apply /usr/src/kernel_compiler_patch/more-ISA-levels-and-uarches-for-kernel-6.8-rc4+.patch;
+git apply /usr/src/linux-amd-zen1-zen2-zen3-openSUSE_TW/more-ISA-levels-and-uarches-for-kernel-6.8-rc4+.patch;
 make oldconfig;
 make nconfig;
 read -p "Ctrl-C now to break out, or press any key to build the kernel..."
-sudo CXXFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong" CFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong" KCFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong" make -j $(($(nproc) + 1))
-; make modules_install
-; make install
+CXXFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong" CFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong" KCFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong" make -j $(($(nproc) + 1));
+sudo make modules_install;
+sudo make install;
