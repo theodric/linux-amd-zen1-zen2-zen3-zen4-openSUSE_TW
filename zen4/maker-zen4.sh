1f07887de7cd74d2a5041178da38c0701dd109ff
Theodric <theodric@github.io>
2025-11-24 15:04:16 +0000

    Gibble, Gobble, we ACCEPT YOU ...

--- zen4/maker-zen4.sh
+++ zen4/maker-zen4.sh
@@ -4,9 +4,9 @@ make oldconfig;
 make nconfig;
 read -p "Ctrl-C now to break out, or press any key to build the kernel..."
 
-export CXXFLAGS="-Ofast -pipe -march=znver3 -mtune=znver3 -fstack-protector-strong"
-export CFLAGS="-Ofast -pipe -march=znver3 -mtune=znver3 -fstack-protector-strong"
-export KCFLAGS="-Ofast -pipe -march=znver3 -mtune=znver3 -fstack-protector-strong"
+export CXXFLAGS="-Ofast -pipe -march=znver4 -mtune=znver4 -fstack-protector-strong"
+export CFLAGS="-Ofast -pipe -march=znver4 -mtune=znver4 -fstack-protector-strong"
+export KCFLAGS="-Ofast -pipe -march=znver4 -mtune=znver4 -fstack-protector-strong"
 make -j $(($(nproc) + 1))
 make modules_install
 make install
