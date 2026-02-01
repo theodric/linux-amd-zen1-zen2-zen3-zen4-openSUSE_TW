f06c1616af03bf14f8609f3ae5ccbca3e967c608
Theodric <theodric@github.io>
2026-02-01 13:10:12 +0000

    I want a WESSON OIL lease!!

--- zen1/maker-zen1.sh
+++ zen1/maker-zen1.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-git apply /usr/src/kernel_compiler_patch/more-ISA-levels-and-uarches-for-kernel-6.8-rc4+.patch;
-make oldconfig;
-make nconfig;
-read -p "Ctrl-C now to break out, or press any key to build the kernel..."
+echo applying uarch patch:
+git apply /usr/src/more-ISA-levels-and-uarches-for-kernel-6.16+.patch
+echo done
 
-export CXXFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong"
-export CFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong"
-export KCFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong"
-make -j $(($(nproc) + 1))
-make modules_install
-make install
+echo extracting config:
+zcat /proc/config.gz > .config
+echo done
+
+echo making oldconfig:
+make oldconfig
+echo done
+
+echo entering kernel config:
+make nconfig
+
+echo setting compiler flags:
+#export CXXFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong"
+#export CFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong"
+#export KCFLAGS="-Ofast -pipe -march=znver1 -mtune=znver1 -fstack-protector-strong"
+
+export CXXFLAGS="-Ofast -pipe -march=native -mtune=native -fstack-protector-strong"
+echo CXXFLAGS = "$CXXFLAGS"
+
+export CFLAGS="-Ofast -pipe -march=native -mtune=native -fstack-protector-strong"
+echo CFLAGS = "$CFLAGS"
+
+export KCFLAGS="-Ofast -pipe -march=native -mtune=native -fstack-protector-strong"
+echo KCFLAGS = "$KCFLAGS"
+
+read -p "Build time. Ctrl-C now to break out, or press any key to build the kernel..."
+
+time make -j $(($(nproc) + 1))
+sudo make modules_install
+sudo make install
